3620559183fe9a9b965e5a766147d3d6b0c8351b
Md. Imrul Hasan <sawon.imrul@gmail.com>
2021-10-04 01:16:25 +0600

    -Login UI Design -Registration UI Design

---
+++ app/src/main/java/com/imrul/cookingmaster/RegisterUserActivity.java
@@ -0,0 +1,14 @@
+package com.imrul.cookingmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
