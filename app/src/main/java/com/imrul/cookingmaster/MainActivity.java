bbf108ad5bc45a8e966a8c8e72f16227bbdece68
Md. Imrul Hasan <sawon.imrul@gmail.com>
2021-10-21 15:53:16 +0600

    -HomeScreen -NoActionbar in Theme -

--- app/src/main/java/com/imrul/cookingmaster/MainActivity.java
+++ app/src/main/java/com/imrul/cookingmaster/MainActivity.java
@@ -3,12 +3,14 @@ package com.imrul.cookingmaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView register;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         register = (TextView) findViewById(R.id.register);
+        login = (Button) findViewById(R.id.signIn);
 
         register.setOnClickListener(this);
+        login.setOnClickListener(this);
     }
 
     @Override
@@ -25,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.register:
                 startActivity(new Intent(this, RegisterUserActivity.class));
+                break;
+            case R.id.signIn:
+                startActivity(new Intent(this, HomeActivity.class));
                 break;
 
         }
